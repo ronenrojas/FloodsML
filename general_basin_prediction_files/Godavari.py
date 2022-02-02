fixing many things including scaling and rescaling the data

general_basin_prediction_files/Godavari.py
@@ -78,18 +78,17 @@ class IMDGodavari(Dataset):
         for key in self.sample_to_basin_x.keys():
             start_ind = key[0]
             end_ind = key[1]
-            indices_X, static_features, _, basin_name = self.sample_to_basin_x[key]
-            mu_y, std_y = self.basin_name_to_mean_std_y[basin_name]
+            indices_X, static_features, _ = self.sample_to_basin_x[key]
             if idx in range(start_ind, end_ind):
                 idx = idx - start_ind
                 x_new = copy.deepcopy(self.x[idx: idx + self.seq_length, :, :, :])
                 y_new = copy.deepcopy(self.y[idx + self.seq_length - 1])
-                y_new = ((y_new - mu_y) / std_y)
                 x_new = np.multiply(x_new, indices_X)
                 x_new = np.reshape(x_new, (x_new.shape[0], x_new.shape[1], x_new.shape[2] * x_new.shape[3]))
-                for i in range(x_new.shape[1]):
-                    x_new[:, i, :] -= self.min_values[i]
-                    x_new[:, i, :] /= (self.max_values[i] - self.min_values[i])
+                if self.period == 'train':
+                    for i in range(x_new.shape[1]):
+                        x_new[:, i, :] -= self.min_values[i]
+                        x_new[:, i, :] /= (self.max_values[i] - self.min_values[i])
                 x_new = np.reshape(x_new, (x_new.shape[0], x_new.shape[1] * x_new.shape[2]))
                 if self.include_static:
                     static_features = static_features[np.newaxis, :]
@@ -130,29 +129,30 @@ class IMDGodavari(Dataset):
             # over all the samples (timestamps) + H_LAT + W_LON per channel
             self.min_values = self.x.min(axis=0).min(axis=1).min(axis=1)
             self.max_values = self.x.max(axis=0).max(axis=1).max(axis=1)
-            indices_Y, y = self.preprocessor.get_basin_indices_y(basin, start_date, end_date)
-            mu_y = y.mean()
-            std_y = y.std()
-            self.basin_name_to_mean_std_y[basin] = (mu_y, std_y)
+            y = self.preprocessor.get_basin_indices_y(basin, start_date, end_date)
+            if self.period == 'train':
+                mu_y = y.mean()
+                std_y = y.std()
+                y = ((y - mu_y) / std_y)
+                self.basin_name_to_mean_std_y[basin] = (mu_y, std_y)
+            indices_to_include_months_single_basin, y_new = self.extract_from_data_by_months(y, start_date, end_date, basin_name=basin)
+            num_indices = len(indices_to_include_months_single_basin)
             if i == 0:
-                self.y = y
+                self.y = y_new
             else:
-                self.y = np.concatenate([self.y, y], axis=0)
-            indices_to_include_months = self.get_monthly_data(data, y, start_date, end_date)
+                indices_to_include_months += np.array(num_indices)
+                self.y = np.concatenate([self.y, y_new], axis=0)
             self.num_samples = len(self.basin_list) * len(indices_to_include_months)
             self.sample_to_basin_x[(i * (len(indices_to_include_months) - self.seq_length + 1),
                                     (i + 1) * (len(indices_to_include_months) - self.seq_length + 1))] = \
-                (indices_X_time_features, static_features, indices_to_include_months, basin)
+                (indices_X_time_features, static_features, indices_to_include_months)
             self.sample_to_basin_y[(i * (len(indices_to_include_months) - self.seq_length + 1),
                                     (i + 1) * (len(indices_to_include_months) - self.seq_length + 1))] = \
-                (mu_y, std_y)
+                basin
             if not self.include_static:
                 self.num_attributes = 0
         if len(indices_to_include_months) > 0:
             self.x = self.x[indices_to_include_months, :, :, :]
-            self.y = self.y[indices_to_include_months]
-        # if self.period == 'train':
-        #     self.y = self.local_rescale(self.y)
         print("Data set for {0} for basins: {1}".format(self.period, self.basin_list))
         print("Number of attributes should be: {0}".format(self.num_attributes))
         print("Number of features should be: num_features + num_attributes= {0}".format(
@@ -170,27 +170,30 @@ class IMDGodavari(Dataset):
             x_static = None
         return indices_X, x_static
 
-    def local_rescale(self, feature: np.ndarray) -> np.ndarray:
-        """
-        Rescale output features with local mean/std.
-          param mean_std:
-          param feature: Numpy array containing the feature(s) as matrix.
-          param variable: Either 'inputs' or 'output' showing which feature will
-          be normalized
-        :return: array containing the normalized feature
-        """
-        n_basins = len(self.basin_list)
-        idx = int(len(feature) / n_basins)
-        for i, basin_name in enumerate(self.basin_list):
-            if i == 0:
-                y = feature[i * idx:(i + 1) * idx]
-                y = (y * self.basin_name_to_mean_std_y[basin_name][1]) + self.basin_name_to_mean_std_y[basin_name][0]
-            else:
-                y_temp = feature[i * idx:(i + 1) * idx]
-                y_temp = (y_temp * self.basin_name_to_mean_std_y[basin_name][1]) + \
-                         self.basin_name_to_mean_std_y[basin_name][0]
-                y = np.concatenate([y, y_temp])
+    def revert_normalization_y(self, feature: np.ndarray, basin_name="") -> np.ndarray:
+        y = feature
+        y = y * self.basin_name_to_mean_std_y[basin_name][1] + \
+            self.basin_name_to_mean_std_y[basin_name][0]
         return y
+
+    def extract_from_data_by_months(self, y, start_date, end_date, basin_name=""):
+        # reverting normalization to Y (labels) as we
+        if self.period == 'train':
+            y = self.revert_normalization_y(y, basin_name=basin_name)
+        # getting the months for each date
+        date_months = self.get_months_by_dates(start_date, end_date)
+        # Adjusting for sequence length and lead
+        date_months = date_months[(self.seq_length + self.lead - 1):]
+        ind_date_months = [i for i in range(0, len(y) - (self.seq_length + self.lead - 1)) if
+                           date_months[i] in self.months]
+        idx_temp = [idx for idx in ind_date_months]
+        y_new = y[idx_temp]
+        if self.period == 'train':
+            mu_y_temp = y_new.mean()
+            std_y_temp = y_new.std()
+            y_new = (y_new - mu_y_temp) / std_y_temp
+            self.basin_name_to_mean_std_y[self.basin_list[j]] = (mu_y_temp, std_y_temp)
+        return idx_temp, y_new
 
     @staticmethod
     def get_months_by_dates(start_date, end_date):
@@ -199,20 +202,6 @@ class IMDGodavari(Dataset):
         date_range = pd.date_range(start_date_pd, end_date_pd)
         months = [date_range[i].month for i in range(0, len(date_range))]
         return months
-
-    def get_monthly_data(self, x, y, start_date, end_date):
-        # getting the months for each date
-        date_months = self.get_months_by_dates(start_date, end_date)
-        # Adjusting for sequence length and lead
-        date_months = date_months[(self.seq_length + self.lead - 1):]
-        n_samples_per_basin = int(len(x) / len(self.basin_list))
-        ind_date_months = [i for i in range(0, n_samples_per_basin - (self.seq_length + self.lead - 1)) if
-                           date_months[i] in self.months]
-        ind_include = []
-        for j in range(len(self.basin_list)):
-            idx_temp = [idx + j * n_samples_per_basin for idx in ind_date_months]
-            ind_include += idx_temp
-        return ind_include
 
     def get_min(self):
         return self.min_values

general_basin_prediction_files/cnn_lstm_general.py
@@ -17,6 +17,7 @@ from LSTM import CNNLSTM
 from preprocess_data import Preprocessor
 from Godavari import IMDGodavari
 from captum.attr import IntegratedGradients
+import seaborn as sns
 
 
 root_dir = str(Path(os.getcwd()).parent)
@@ -166,13 +167,13 @@ def calc_bias(obs: np.array, sim: np.array) -> float:
     :return: NSE value.
     """
     bias_95 = None
-    if (np.percentile(obs, 95) != 0):
+    if np.percentile(obs, 95) != 0:
         bias_95 = (np.percentile(sim, 95) - np.percentile(obs, 95)) / np.percentile(obs, 95) * 100
     bias_5 = None
-    if (np.percentile(obs, 5) != 0):
+    if np.percentile(obs, 5) != 0:
         bias_5 = (np.percentile(sim, 5) - np.percentile(obs, 5)) / np.percentile(obs, 5) * 100
     mean_bias = None
-    if (np.nanmean(obs) != 0):
+    if np.nanmean(obs) != 0:
         mean_bias = (np.nanmean(sim) - np.nanmean(obs)) / np.nanmean(obs) * 100
 
     return bias_95, bias_5, mean_bias
@@ -331,7 +332,7 @@ def main():
     for i in range(n_epochs):
         train_epoch(device, model, optimizer, tr_loader, loss_func, i + 1)
         obs, preds = eval_model(device, model, test_loader)
-        preds = ds_test.local_rescale(preds.cpu().numpy())
+        preds = ds_test.revert_normalization_y(preds.cpu().numpy())
         nse = calc_nse(obs.numpy(), preds)
         tqdm.tqdm.write(f"Test NSE: {nse:.3f}")
         model_name = "epoch_{:d}_nse_{:.3f}.ckpt".format(i + 1, nse)
@@ -362,7 +363,7 @@ def main():
                          include_static=include_static)
     val_loader = DataLoader(ds_val, batch_size=2048, shuffle=False)
     obs, preds = eval_model(device, model, val_loader)
-    preds = ds_val.local_rescale(preds.cpu().numpy())
+    preds = ds_val.revert_normalization_y(preds.cpu().numpy())
     obs = obs.numpy()
     nse = calc_nse(obs, preds)
     pb95, pb5, total_b = calc_bias(obs, preds)

general_basin_prediction_files/preprocess_data.py
@@ -58,7 +58,7 @@ class Preprocessor:
                 if "height" in dims_json.keys():
                     image_height = dims_json["height"]
         except Exception as e:
-            print("There was an exception in reading the dims file: {}".format(e))
+            print("There was an exception in reading the dimensions details file: {}".format(e))
         data_ret = np.fromfile(data_file_path).reshape((self.data_len, self.num_channels,
                                                         image_width, image_height))
         return data_ret, image_width, image_height
@@ -156,7 +156,7 @@ class Preprocessor:
                              Preprocessor.get_index(data_discharge, end_date)
         indices_Y = np.linspace(idx_start, idx_end + 1, 1)
         y = np.array(data_discharge[3][idx_start:idx_end + 1])
-        return indices_Y, y
+        return y
 
     def get_index_by_date(self, date_in):
         start_date_pd = pd.to_datetime(datetime.datetime(self.data_start_date[0], self.data_start_date[1],